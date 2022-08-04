85099808d1b8c1498058f1398630eb06199c4fec
Augustin82 <augustin.ragon@gmail.com>
2022-08-04 12:27:19 +0200

    feat: search bar supports label/hints

--- src/BetaGouv/DSFR/SearchBar.elm
+++ src/BetaGouv/DSFR/SearchBar.elm
@@ -1,11 +1,12 @@
 module BetaGouv.DSFR.SearchBar exposing (searchBar, searchBarLG)
 
-import Accessibility exposing (Html, formWithListeners, inputText, label, text)
+import Accessibility exposing (Html, div, formWithListeners, inputText, label, span, text)
 import Accessibility.Landmark
 import BetaGouv.DSFR.Button
 import Html.Attributes as Attr exposing (class)
-import Html.Attributes.Extra exposing (empty)
+import Html.Attributes.Extra exposing (attributeMaybe, empty)
 import Html.Events as Events
+import Html.Extra exposing (nothing, viewMaybe)
 import Json.Decode as Decode
 
 
@@ -14,8 +15,21 @@ type Size
     | LG
 
 
-genericSearchBar : Size -> { submitMsg : msg, inputMsg : String -> msg, buttonLabel : String, inputLabel : String, inputPlaceholder : String, inputId : String, inputValue : String } -> Html msg
-genericSearchBar size { submitMsg, inputMsg, buttonLabel, inputLabel, inputPlaceholder, inputId, inputValue } =
+type alias MandatoryConfig msg =
+    { submitMsg : msg
+    , inputMsg : String -> msg
+    , buttonLabel : String
+    , inputLabel : String
+    , inputPlaceholder : Maybe String
+    , inputId : String
+    , inputValue : String
+    , hints : List (Html msg)
+    , fullLabel : Maybe (Html msg)
+    }
+
+
+genericSearchBar : Size -> MandatoryConfig msg -> Html msg
+genericSearchBar size { submitMsg, inputMsg, buttonLabel, inputLabel, inputPlaceholder, inputId, inputValue, hints, fullLabel } =
     let
         sizeAttr =
             case size of
@@ -24,41 +38,63 @@ genericSearchBar size { submitMsg, inputMsg, buttonLabel, inputLabel, inputPlace
 
                 LG ->
                     class "fr-search-bar--lg"
+
+        hint =
+            case hints of
+                [] ->
+                    nothing
+
+                _ ->
+                    span [ Attr.class "fr-hint-text" ] hints
     in
-    formWithListeners
-        [ class "fr-search-bar"
-        , Attr.id <| "header-" ++ inputId
-        , Accessibility.Landmark.search
-        , sizeAttr
-        , Events.onSubmit submitMsg
-        ]
-        [ label
-            [ class "fr-label"
-            , Attr.for inputId
+    div [ class "flex flex-col gap-2"]
+        [ case fullLabel of
+            Just l ->
+                label
+                    [ Attr.class "fr-label"
+                    , Attr.for inputId
+                    ]
+                    [ l, hint ]
+
+            Nothing ->
+                hint
+        , formWithListeners
+            [ class "fr-search-bar"
+            , Attr.id <| "header-" ++ inputId
+            , Accessibility.Landmark.search
+            , sizeAttr
+            , Events.onSubmit submitMsg
             ]
-            [ text inputLabel ]
-        , inputText inputValue
-            [ class "fr-input"
-            , Attr.type_ "search"
-            , Attr.id inputId
-            , Attr.name inputId
-            , Attr.placeholder inputPlaceholder
-            , Events.onInput inputMsg
-            , Events.on "search" <|
-                Decode.succeed submitMsg
+            [ label
+                [ class "fr-label"
+                , Attr.for inputId
+                ]
+                [ text inputLabel
+                ]
+            , inputText inputValue
+                [ class "fr-input"
+                , Attr.type_ "search"
+                , Attr.id inputId
+                , Attr.name inputId
+                , inputPlaceholder
+                    |> attributeMaybe Attr.placeholder
+                , Events.onInput inputMsg
+                , Events.on "search" <|
+                    Decode.succeed submitMsg
+                ]
+            , DSFR.Button.new { onClick = Nothing, label = buttonLabel }
+                |> DSFR.Button.submit
+                |> DSFR.Button.withAttrs [ Attr.title buttonLabel ]
+                |> DSFR.Button.view
             ]
-        , DSFR.Button.new { onClick = Nothing, label = buttonLabel }
-            |> DSFR.Button.submit
-            |> DSFR.Button.withAttrs [ Attr.title buttonLabel ]
-            |> DSFR.Button.view
         ]
 
 
-searchBar : { submitMsg : msg, inputMsg : String -> msg, buttonLabel : String, inputLabel : String, inputPlaceholder : String, inputId : String, inputValue : String } -> Html msg
+searchBar : MandatoryConfig msg -> Html msg
 searchBar =
     genericSearchBar MD
 
 
-searchBarLG : { submitMsg : msg, inputMsg : String -> msg, buttonLabel : String, inputLabel : String, inputPlaceholder : String, inputId : String, inputValue : String } -> Html msg
+searchBarLG : MandatoryConfig msg -> Html msg
 searchBarLG =
     genericSearchBar LG
